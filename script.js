910ce5f756df20668bf194ce8ea75a3c22d9a2f7
Pepecode22 <Jos.sepulvedaa@gmail.com>
2025-05-05 16:23:04 -0400

    Corrección

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+ //Cambio de colores al azar
 function getRandomColor() {
     const colors = ['green', 'blue', 'red'];
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
-}
+  }
   
+  // Función para cambiar el color de un elemento h5 al hacer clic
+  function changeColorOnClick(event) {
+    if (event.target.tagName === 'H5') {
+      event.target.style.color = getRandomColor();
+    }
+  }
   
-function changeColorOnClick(event) {
-if (event.target.tagName === 'H5') {
-    event.target.style.color = getRandomColor();
-}
-}
-
+  // Event listener
+document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('click', changeColorOnClick);
+  });
